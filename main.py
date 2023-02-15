435a03b699031a76750806cd5642e07f3ccc87fb
tesag <>
2023-02-15 09:45:34 +0000

    For loop cpp code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ L = ["for(int i=0; i<10; i++){ cout<<i<<endl"]
 file1.writelines(L)
 file1.close()
 commitMessage = 'For loop cpp code'
-process = subprocess.call(['./pushToGh.sh', commitMessage], shell=True)
+process = subprocess.call(['bash','./pushToGh.sh', commitMessage])
 # process = subprocess.call(['chmod +x ./pushToGh.sh', commit_message], shell=True)
 
 print(process)
--- pushToGh.sh
+++ pushToGh.sh
@@ -1,9 +1,10 @@
 # !/bin/bash
 # lines that start like this are shell comments
 # read projects current directory with $PWD
-echo “running command from” $PWD
+echo “running command from” $PWD 
+echo “running command from” $0 $1
 cd $PWD
 git add .
-git commit -am “$1”
+git commit -am "$1"
 git push
 exit 1
