c71feaff0ea8a101f263c352740685a4e4eb20dc
tesag <>
2023-02-15 09:21:40 +0000

    “”

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ file1 = open("forloop.cpp", "w")
 L = ["for(int i=0; i<10; i++){ cout<<i<<endl"]
 file1.writelines(L)
 file1.close()
-commitMessage = "For loop cpp code"
+commitMessage = 'For loop cpp code'
 process = subprocess.call(['./pushToGh.sh', commitMessage], shell=True)
 # process = subprocess.call(['chmod +x ./pushToGh.sh', commit_message], shell=True)
 
